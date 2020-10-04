bf8f1e31bc8fb6d237096c8b2ef2870008f0c256
Ankur Garg <56405200+ankur2811@users.noreply.github.com>
2020-10-04 10:46:58 -0700

    Create Solution2.java

---
+++ Day1/Solution2.java
@@ -0,0 +1,21 @@
+class RecentCounter {
+    LinkedList<Integer> que;
+       public RecentCounter() {
+           que=new LinkedList<>();
+           
+       }
+       
+       public int ping(int t) {
+           que.addLast(t);
+           while((que.get(que.size()-1)-que.get(0))>3000)
+               que.removeFirst();
+           
+           return que.size();
+       }
+   }
+   
+   /**
+    * Your RecentCounter object will be instantiated and called as such:
+    * RecentCounter obj = new RecentCounter();
+    * int param_1 = obj.ping(t);
+    */
